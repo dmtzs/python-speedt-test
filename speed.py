de55b73e3dfd2667ccae6635272ec08bab1f69a8
Diego Martínez Sánchez <diego.mtzs00@gmail.com>
2022-05-10 22:05:47 -0500

    Update speed.py
    Added more visual functionalities to the results and actions that the script is doing

--- speed.py
+++ speed.py
@@ -1,25 +1,70 @@
 # pip install speedtest-cli
 # Measures in bits per second
+try:
+    import os
+    import time
+    import platform
+    from tqdm import tqdm
+    import speedtest as st
+    from colorama import Fore, init
+except ImportError as eImp:
+    print(f"The following import error ocurred: {eImp}")
 
-import speedtest as st
+init(autoreset=True)
+
+def clear_screen():
+    actual_system = platform.system()
+
+    if actual_system == "Windows":
+        os.system("cls")
+    elif actual_system == "Linux":
+        os.system("clear")
+    else:
+        print("The script only runs in linux and windows os systems")
+        exit()
+
+def print_results(data):
+    print("\n\n")
+    print(Fore.MAGENTA + "="*80)
+    print(Fore.GREEN + "INTERNET SPEED TEST RESULTS:".center(80))
+    print(Fore.MAGENTA + "="*80)
+    print(Fore.CYAN +
+        f"Download: {data['download']} | Upload:{data['upload']} | Ping: {data['ping']}".center(80))
+    print(Fore.MAGENTA + "-"*80)
+    print(Fore.CYAN +
+        f"HOST:{data['host']} | ISP:{data['isp']} | LATENCY: {data['latency']}".center(80))
+    print(Fore.MAGENTA + "-"*80)
 
 def mainMet():
     data= {}
 
+    print(Fore.GREEN + "GETTING BEST AVAILABLE SERVERS, UPLOADING & DOWNLOADING SPEED.....")
     speed= st.Speedtest()
     speed.get_best_server()
+    for i in tqdm(range(100), colour="green", desc="Finding Optimal Server"):
+        time.sleep(0.005)
 
     data["ping"]= speed.results.ping
     downReal= speed.download(threads=None)
+    for i in tqdm(range(100), colour="cyan", desc="Getting Download Speed"):
+        time.sleep(0.005)
+
     upReal= speed.upload(threads=None, pre_allocate= False)
+    for i in tqdm(range(100), colour="red", desc="Getting Upload Speed"):
+        time.sleep(0.005)
 
     # Conversion to real speed contracted
-    data["download"]= round(downReal / (10**6), 2)
-    data["upload"]= round(upReal / (10**6), 2)
+    data["download"] = round(downReal / (10**6), 2)
+    data["upload"] = round(upReal / (10**6), 2)
+    # print(speed.results)
+    data["host"] = speed.results.server["host"]
+    data["isp"] = speed.results.client["isp"]
+    data["latency"] = speed.results.server["latency"]
 
     del downReal, upReal
 
-    print(data)
+    print_results(data)
     
 if __name__== "__main__":
+    clear_screen()
     mainMet()
